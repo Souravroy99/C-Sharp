ddb0b3b902351122fa31f0d1eac08ec43ee13224
Souravroy99 <souravroy200418@gmail.com>
2025-10-16 19:37:26 +0530

    Methods

---
+++ 02 Methods/MethodsDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿static int fibonacci(int n)
+{
+    if (n <= 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int n = Convert.ToInt32(Console.ReadLine()) ;
+
+int ans = fibonacci(n);
+Console.WriteLine(ans);
